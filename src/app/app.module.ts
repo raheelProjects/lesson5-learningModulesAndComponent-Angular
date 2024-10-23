dcc32822788ad98eb3af9373bc0f26f372a1ebf6
Raheel Jamshaid <76648311+raheelProjects@users.noreply.github.com>
2024-10-23 11:53:54 +0200

    generated components
    generated components to render in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestModuleModule } from './test-module/test-module.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TestModuleModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test-module/test-module.module.ts
+++ src/app/test-module/test-module.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FirstComponentComponent, SecondComponentComponent],
+  imports: [CommonModule],
+  exports: [FirstComponentComponent, SecondComponentComponent],
 })
-export class TestModuleModule { }
+export class TestModuleModule {}
